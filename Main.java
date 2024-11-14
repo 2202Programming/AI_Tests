4a3907f71f8e57eea932709dd80ff4b840f06b26
Ben Grogan <26grogab@elmbrookstudents.org>
2024-11-13 18:27:50 -0600

    code with errors for the rookies

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!")
+        //TODO: I don't know what's going wrong. Can somebody help me?
+    }
+}
